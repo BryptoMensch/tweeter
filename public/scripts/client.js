6fd5159156ae700bf126c20222967f0de7e4243b
BryptoMensch <90989763+BryptoMensch@users.noreply.github.com>
2022-02-04 10:06:26 -0500

    reworked entire javascript/jquery files. Implemented GET and Post requests. Tweets load without having to refresh site.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,23 +1,61 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function () {
+	const createTweetElement = function (tweet) {
+		const layout = `
+		<article id='tweeter-container'>
+		<div class="tweet-container">
+			<div class="tweet-items">
+				<div class="user-attributes">
+					<div class="user-id">
+						<img id="avatar" src="${tweet.user.avatars}" />
+							<div>	<h4 id="name">${tweet.user.name}</h4></div>
+					</div>
+					<div class="user-handle"><h4 id="handle">${tweet.user.handle}</h4></div>
+				</div>
+				<div class="tweet-content">
+					<p id="tweet">${tweet.content.text}</p>
+				</div>
+				<div class="below-tweet">
+					<div class="time-created">${timeago.format(tweet.created_at)}</div>
+					<div class="favicons">
+						<i id="flag" class="fas fa-flag"></i>
+						<i id="retweet" class="fas fa-retweet"></i>
+						<i id="heart" class="fas fa-heart"></i>
+					</div>
+				
+				</div>
+			</div>
+			</div>
+		</div>
+	</article>`;
 
-const tweetData = {
-	user: {
-		name: "Newton",
-		avatars: "https://i.imgur.com/73hZDYK.png",
-		handle: "@SirIsaac",
-	},
-	content: {
-		text: "If I have seen further it is by standing on the shoulders of giants",
-	},
-	created_at: 1461116232227,
-};
+		return layout;
+	};
 
-const $tweet = createTweetElement(tweetData);
+	const renderTweets = function (tweets) {
+		console.log(tweets);
+		for (tweeter of tweets) {
+			console.log(tweeter);
+			$("#tweeter-container").prepend(createTweetElement(tweeter));
+		}
+	};
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$(".tweets").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+	const loadTweets = function () {
+		$.get("/tweets").then((tweet) => {
+			renderTweets(tweet);
+		});
+	};
+
+	$("form").on("submit", (evt) => {
+		evt.preventDefault();
+		const param = $("#tweet-text").serialize();
+		$.post("/tweets", param).then(() => {
+			loadTweets();
+		});
+		$("form").trigger("reset");
+	});
+	loadTweets();
+});
+
+// Left to do
+// 1. Error Handling
+// 2.
