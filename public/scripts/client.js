e13db2068bc49e77f1a68215f100d77c5f281034
BryptoMensch <90989763+BryptoMensch@users.noreply.github.com>
2022-02-05 11:47:20 -0500

    cleaned up code, added descriptions. ran final tests.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,4 @@
+// Creates framework to append new tweets to using jQuery and syntax literals.
 $(document).ready(function () {
 	const createTweetElement = function (tweet) {
 		const layout = `
@@ -21,7 +22,6 @@ $(document).ready(function () {
 						<i id="retweet" class="fas fa-retweet"></i>
 						<i id="heart" class="fas fa-heart"></i>
 					</div>
-
 				</div>
 			</div>
 			</div>
@@ -31,66 +31,52 @@ $(document).ready(function () {
 		return layout;
 	};
 
+	// Renders news tweets, appends them to above framework.
 	const renderTweets = function (tweets) {
 		for (tweeter of tweets) {
 			$("#tweeter-container").prepend(createTweetElement(tweeter));
 		}
 	};
 
-	// $(".error-message").hide();
-
+	// Submits form with jQuery.
 	$("form").on("submit", (evt) => {
 		evt.preventDefault();
 
+		//Validates tweets using two conditionals.
 		const errorMsg = $(".error-message");
 		const counterError = $(".counter");
 
+		//Returns error message if there is nothing in form field.
 		if (counterError.val() >= 140) {
 			errorMsg.text("Your message should contain text.");
 			errorMsg.fadeIn(1000).delay(1000).fadeOut(1000);
 			$("form").trigger("reset");
 		}
+
+		// Returns error message if tweet is over character limit.
 		if (counterError.val() <= 0) {
-			// $(".counter").trigger("reset");
 			errorMsg.text("Your message has too many characters.");
 			errorMsg.fadeIn("slow").delay(2000).fadeOut("slow");
 
+			// Resets counter accordingly
 			$(".counter").get(0).reset();
 		}
-		const param = $("#tweet-text").serialize();
 
+		// POST handler.
+		const param = $("#tweet-text").serialize();
 		$.post("/tweets", param).then(() => {
 			loadTweets();
 		});
 		$("form").trigger("reset");
 		$(".counter").text(140);
 	});
+
+	// GET handler.
 	const loadTweets = function () {
 		$.get("/tweets").then((tweet) => {
 			console.log(tweet);
 			renderTweets(tweet);
 		});
 	};
-
 	loadTweets();
 });
-
-// Left to do
-// 1. Error Handling
-// 2. Warning error VM1009
-
-// $(".error-message").hide("slow");
-// const input = $("#tweet-text").val();
-// //
-
-// if (input === "") {
-// 	$(".error-message").text("Tweets cannot be empty.").show("slow");
-// } else if (input.length > 140) {
-// 	$(".error-message").text("Tweets cannot be empty.").show("slow");
-
-// //
-// .then($("form", "counter").trigger("reset"));
-
-// //
-// $(".error-message").hide();
-// $(".new-tweet").hide();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,8 @@
+// Character counter helper function.
+// > 140, text turns red. >=, text stays/reverts to regular text color.
 $(document).ready(function () {
 	$("#tweet-text").on("keyup", function () {
 		const maxLength = 140;
-		// console.error(this);
 		let length = maxLength - $(this).val().length;
 		if (length < 0) {
 			$(".counter").css("color", "red");
